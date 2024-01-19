8f4f34a1b284753c4df4581a5d102834d630e112
Adam Butler <adam@labfoo.dev>
2024-01-19 12:37:55 +0000

    Add basic dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def greet
+    case Time.zone.now.hour
+    when 4..11 then "Good morning"
+    when 12..17 then "Good afternoon"
+    when 18..23 then "Good evening"
+    else
+      "Welcome"
+    end
+  end
 end
