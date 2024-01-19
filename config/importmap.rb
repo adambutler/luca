08afb51de72440a69bbb75264c117169a374139f
Adam Butler <adam@labfoo.dev>
2024-01-19 11:01:19 +0000

    Add Polaris ViewComponents

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "polaris-view-components", to: "polaris_view_components.js"
