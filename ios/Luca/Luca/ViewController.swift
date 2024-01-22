213cce5042217bfdb982f0dd64110b741081b6b0
Adam Butler <adam@labfoo.dev>
2024-01-22 12:38:10 +0000

    Bootstrap iOS project

---
+++ ios/Luca/Luca/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Luca
+//
+//  Created by Adam Butler on 22/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
